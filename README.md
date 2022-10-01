7229c3506fe17f2d11eac6676e779dfa6023f5e1
Abhishek Raj <abhishekraj100001@gmail.com>
2022-10-01 23:26:24 +0530

    add increase quantity function

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Getting Started with Create React App
+This is a Cart made using React
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,45 +1,63 @@
-import React from 'react';
+import React from "react";
 
 class CartItem extends React.Component {
-    constructor() {
-        super()
-        this.state={
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-    }
-    render () {
-        const {price,title,qty} = this.state;
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "Phone",
+      qty: 1,
+      img: "",
+    };
+//    this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+  increaseQuantity = ()=> {
+    console.log('this',this.state)
+    
+  }
+  render() {
+    const { price, title, qty } = this.state;
     return (
-        <div className="cart-item" >
+      <div className="cart-item">
         <div className="left-block">
-        <img style={styles.image}/>
-        
+          <img style={styles.image} />
         </div>
 
-        <div className='right-block'>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}> {title} </div>
+          <div style={{ color: "#777" }}> Rs. {price} </div>
+          <div style={{ color: "#777" }}> {qty} </div>
+          <div className="cart-item-actions">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+              alt="minus"
+              className="action-icons"
+            />
 
-           <div style = {{fontSize:25}}> {title} </div>
-           <div style = {{color:'#777'}}> Rs. {price} </div>
-           <div style = {{color:'#777'}}> {qty} </div>
-           <div className='cart-item-actions'>
-<img src="https://cdn-icons-png.flaticon.com/512/992/992683.png" alt="minus" className='action-icons'/>
-<img src="https://cdn-icons-png.flaticon.com/512/992/992651.png" alt="plus" className='action-icons'/>
-<img src="https://cdn-icons-png.flaticon.com/512/484/484662.png" alt="delete" className='action-icons'/>
-           </div>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+              alt="plus"
+              className="action-icons"
+              onClick={this.increaseQuantity}
+            />
+
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/484/484662.png"
+              alt="delete"
+              className="action-icons"
+            />
+          </div>
         </div>
-        </div>
-    )
-    }
+      </div>
+    );
+  }
 }
 const styles = {
-    image:{
-        height:110,
-        width:110,
-        borderRadius:4,
-        background:'#ccc'
-    }
-}
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
 export default CartItem;
